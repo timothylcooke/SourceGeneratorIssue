a845b4adacfd8e29bb126c3580046c00476e4473
Tim Cooke <tcooke@greateyecare.com>
2024-12-11 15:35:12 -0500

    Adding this namespace reference breaks only the SDK-style project

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -6,8 +6,14 @@ public partial class App : Application
 {
     private void Application_Startup(object sender, StartupEventArgs e)
     {
+        // This line makes a compiler error in LegacyApp, but builds in SdkStyleApp!
+        // This error will not show up if any ReactiveAttributes are added to any fields.
+        // error CS0234: The type or namespace name 'SourceGenerators' does not exist in the namespace 'ReactiveUI' (are you missing an assembly reference?)
         var reactiveAttributeTypeViaCompiler = typeof(ReactiveUI.SourceGenerators.ReactiveAttribute);
 
+        // This works fine in both the LegacyApp *and* SdkStyleApp (if you comment out all references to SourceGenerators attributes/properties to get the project to build).
         var reactiveAttributeTypeViaReflection = GetType().Assembly.GetType("ReactiveUI.SourceGenerators.ReactiveAttribute");
+
+        // The type is clearly getting compiled, but anything that references it (e.g. adding attributes to a ViewModel) fails to build.
     }
 }
--- src/ExampleViewModel.cs
+++ src/ExampleViewModel.cs
@@ -6,9 +6,13 @@ public partial class ExampleViewModel : ReactiveObject
 {
     public ExampleViewModel()
     {
+        // Compiler error in LegacyApp (If ReactiveAttribute is not added to field):
+        // error CS0103: The name 'MyIntProperty' does not exist in the current context
         MyIntProperty = 3;
     }
 
+    // Compiler error in LegacyApp:
+    // error CS0234: The type or namespace name 'SourceGenerators' does not exist in the namespace 'ReactiveUI' (are you missing an assembly reference?)
     [ReactiveUI.SourceGenerators.Reactive]
     private int _myIntProperty;
 }
