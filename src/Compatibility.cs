b8cd353452627a1b1ff9fc99975b3113d8dc5737
Tim Cooke <tcooke@greateyecare.com>
2024-12-12 14:29:46 -0500

    Using PolySharp does not change the behavior

--- src/Compatibility.cs
+++
@@ -1,8 +0,0 @@
-﻿#if !NET5_0_OR_GREATER
-
-namespace System.Runtime.CompilerServices;
-
-// This class is necessary for anything older than net5.0 that references ReactiveUI.SourceGenerators
-internal static class IsExternalInit { }
-
-#endif
